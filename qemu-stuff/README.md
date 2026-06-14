63ae6901d0b9f14f29ec940d134108d62dd40f2a
jellou_ <liamprogames14@gmail.com>
2026-06-14 15:28:53 -0400

    Add files via upload

---
+++ qemu-stuff/README.md
@@ -0,0 +1,8 @@
+# this qemu-stuff folder is for me to emulate Raspberry Pi OS Legacy Lite 64 bit on QEMU
+
+so if you're already on like ARM64 gtfo of this folder :)
+
+anyways here's the links to the resources:
+https://github.com/luisespino/assembly/tree/main/emulate-raspios-arm64-on-qemu, and yes this link is spanish but whatever
+
+https://www.raspberrypi.com/software/operating-systems/, for this one scroll down and click Download on Raspberry Pi OS Legacy Lite 64 bit
---
+++ qemu-stuff/raspberry.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# copied from: https://github.com/luisespino/assembly/tree/main/emulate-raspios-arm64-on-qemu, only thing i changed was the ^ to \ , and changed the path, and added #!/bin/bash (and added these comments but do i really need to say that?)
+# also why the fuck is the echo the only english part of the README
+"/mnt/c/Program Files/qemu/qemu-system-aarch64.exe" \
+  -M raspi3b \
+  -cpu cortex-a53 \
+  -m 1G -smp 4 \
+  -kernel kernel8.img \
+  -sd raspios.img  \
+  -dtb bcm2710-rpi-3-b-plus.dtb \
+  -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200
+  dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+  -serial stdio \
+  -usb -device usb-mouse -device usb-kbd \
+  -device usb-net,netdev=net0 \
+  -netdev user,id=net0,hostfwd=tcp::5555-:22
+
+echo QEMU is finished
