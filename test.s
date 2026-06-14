95f30d2f52508f715ace9e6c705ce8bd16a7694c
jellou_ <liamprogames14@gmail.com>
2026-06-14 18:15:02 -0400

    Add message printing in assembly code

--- test.s
+++ test.s
@@ -1,8 +1,21 @@
 .global _start
 
+.section .text
 _start:
-	mov x0, 5
+
+	// print
+    mov x0, #1
+    ldr x1, =msg
+    mov x2, #len
+    mov x8, 64
+    svc 0
+
+	// returns 0 (success)
+	mov x0, 0
 
 	mov x8, 93
 	svc 0
-	// this should return 5 i think
+.section .rodata
+msg:
+	.ascii "Hello, World!\n"
+	len = . - msg
